3ccef8f2952e86aa39b5ad785d4e56e434008586
Anis <anis.lounis@concordia.ca>
2025-02-07 14:21:01 -0500

    Added random delays between chall restarts

--- source/chall/socat_server.sh
+++ source/chall/socat_server.sh
@@ -5,5 +5,5 @@ SCRIPT_PATH="/home/chall/little-squid-game.py"
 
 while true; do
     socat TCP-LISTEN:$PORT,reuseaddr EXEC:$SCRIPT_PATH,pty
-    sleep 1
+    sleep $(shuf -i 1000-2000 -n 1 | awk '{print $1/1000}')
 done
